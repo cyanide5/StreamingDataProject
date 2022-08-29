4c74abf3e08e765ce88e6aaec2829994f28d8441
namex <namexremoved@gmil.com>
2022-08-29 08:08:12 -0500

    cleaned up old code that was causing an error.

--- app/apis_v1/agg.py
+++ app/apis_v1/agg.py
@@ -12,4 +12,4 @@ api = Namespace("v1/agg", description="netflix data")
 class RatingsAggResource(Resource):
     @api.doc(responses={500: '{column} is not a supported agg type'})
     def get(self):
-        return agg_data('taco')
+        return agg_data('rating')
