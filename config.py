3c0df4595b42d97579158c7c961644a9e6015734
namex <namexremoved@gmil.com>
2022-08-28 21:26:41 -0500

    removed Dockerfile removed unused database.ini

--- config.py
+++ config.py
@@ -3,13 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-database_config = ConfigParser()
-database_config.read('database.ini')
-
-# host = database_config['postgres']['host']
-# database = database_config['postgres']['database']
-# user = database_config['postgres']['user']
-# password = database_config['postgres']['password']
 
 host = 'localhost'
 database = 'streaming_services'
