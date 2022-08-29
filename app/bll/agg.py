b78106a7943b1cab34b7641f008c0d881c105be0
namex <namexremoved@gmil.com>
2022-08-28 21:47:07 -0500

    added comment

--- app/bll/agg.py
+++ app/bll/agg.py
@@ -2,4 +2,6 @@ from app.dll.netflix_agg import data_count
 
 
 def agg_data(column):
+    # hulu calls can be configured in DLL
+    # data then parsed and aggregated here
     return data_count(column)
