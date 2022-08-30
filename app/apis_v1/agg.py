c81ea73af80f4b7142b7434a598c35dec5f17d95
namex <namexremoved@gmil.com>
2022-08-30 13:25:45 -0500

    updated agg description

--- app/apis_v1/agg.py
+++ app/apis_v1/agg.py
@@ -3,7 +3,7 @@ import logging
 from flask_restx import Namespace, Resource
 from app.bll.agg import agg_data
 
-api = Namespace("v1/agg", description="netflix data")
+api = Namespace("v1/agg", description="Aggregate Summary Data")
 
 
 @api.route('/ratings/')
